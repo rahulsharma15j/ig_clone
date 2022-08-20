d9187c57925ebdfc7339cb40b889bc56fab7ee33
Rahul <rahulsharma15j@gmail.com>
2022-08-20 16:58:47 +0530

    post

---
+++ data/post.js
@@ -0,0 +1,154 @@
+import { USERS } from "./users";
+
+export const POSTS = [
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+    user: USERS[0].user,
+    likes: 3423,
+    caption: "slkdfjslkdfjlksdjflksdfjlksdjfkljkl",
+    profile_picture: USERS[0].image,
+    comments: [
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+    ],
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+    user: USERS[0].user,
+    likes: 3423,
+    caption: "slkdfjslkdfjlksdjflksdfjlksdjfkljkl",
+    profile_picture: USERS[0].image,
+    comments: [
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+    ],
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+    user: USERS[0].user,
+    likes: 3423,
+    caption: "slkdfjslkdfjlksdjflksdfjlksdjfkljkl",
+    profile_picture: USERS[0].image,
+    comments: [
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+    ],
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+    user: USERS[0].user,
+    likes: 3423,
+    caption: "slkdfjslkdfjlksdjflksdfjlksdjfkljkl",
+    profile_picture: USERS[0].image,
+    comments: [
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+    ],
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+    user: USERS[0].user,
+    likes: 3423,
+    caption: "slkdfjslkdfjlksdjflksdfjlksdjfkljkl",
+    profile_picture: USERS[0].image,
+    comments: [
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+    ],
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+    user: USERS[0].user,
+    likes: 3423,
+    caption: "slkdfjslkdfjlksdjflksdfjlksdjfkljkl",
+    profile_picture: USERS[0].image,
+    comments: [
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+      {
+        user: "kflsdjklsd",
+        comment:
+          "skldfjklsdfjklsdfklsdjflksdjfklsdflksdjfklsdjfkljsdklfjklsdfklsdfklsdjfklsdjkl",
+      },
+    ],
+  },
+];
---
+++ data/users.js
@@ -0,0 +1,37 @@
+export const USERS = [
+  {
+    user: "cleverqazi",
+    image:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    user: "cleverqazisdfsdfsdf",
+    image:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    user: "cleverqazi",
+    image:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    user: "cleverqazi",
+    image:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    user: "cleverqazi",
+    image:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    user: "cleverqazi",
+    image:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    user: "cleverqazi",
+    image:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+];
